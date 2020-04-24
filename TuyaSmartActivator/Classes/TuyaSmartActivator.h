0545a4b6f9bc58c6c7fe4c09318088e9d3451615
huangdaxia <huangkai@tuya.com>
2020-04-24 20:57:38 +0800

    feat: 支持 ble + wifi 类配网

--- TuyaSmartActivator/Classes/TuyaSmartActivator.h
+++ TuyaSmartActivator/Classes/TuyaSmartActivator.h
@@ -7,12 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "TYBLEAdvModel.h"
 
 typedef enum : NSUInteger {
-    TYActivatorModeEZ, // smart config mode (EZ)
+    TYActivatorModeEZ, // fast connect mode (EZ)
     TYActivatorModeAP, // hotspot mode (AP)
 } TYActivatorMode;
-
 
 @interface TuyaSmartActivator : NSObject
 
@@ -43,5 +43,36 @@ typedef enum : NSUInteger {
  */
 - (void)stopConfigWiFi;
 
+
+#pragma mark - BLE + Wi-Fi config
+
+/// start scan un active ble device
+///
+/// @param discoveryHandler  block when discovery device
+- (void)startDiscovery:(void(^)(TYBLEAdvModel *model))discoveryHandler;
+
+
+/// stop ble discovery
+- (void)stopDiscovery;
+
+
+/// active discoveryed ble device
+///
+/// @param advModel ble device
+/// @param authKey the device authkey
+/// @param random  the device random data
+/// @param ssid router name
+/// @param password router password
+/// @param token token
+- (void)startConfigBLEWifiWithAdvModel:(TYBLEAdvModel *)advModel
+                               authKey:(NSString *)authKey
+                                random:(NSString *)random
+                                  ssid:(NSString *)ssid
+                              password:(NSString *)password
+                                 token:(NSString *)token;
+
+
+/// stop ble wifi device configuration
+- (void)stopConfigBLEWifiWithAdvModel:(TYBLEAdvModel *)advModel;
 
 @end
